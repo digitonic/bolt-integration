601e08ac9b769fcf3ff42963a04a0efd1d945250
MrTammer <juanick@hotmail.fr>
2020-03-17 09:23:23 +0000

    Added Trait public methods to interface

--- src/Contracts/HasTenancies.php
+++ src/Contracts/HasTenancies.php
@@ -11,4 +11,10 @@ interface HasTenancies
     public function boltKeywords(): array;
 
     public function boltSenders(): array;
+
+    public function getContactsFromCentralisedOptouter(): array;
+
+    public function getPhoneNumbersFromCentralisedOptouter(string $table): array;
+
+    public function getTablesFromCentralisedOptouter(): array;
 }
